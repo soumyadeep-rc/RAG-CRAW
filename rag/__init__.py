8999643dd8a911c0f5bc19bc4a9a358e45ab9c99
Soumyadeep Roy Chowdhury <soumyadeeproychowdhury101@gmail.com>
2026-03-28 14:44:26 +0530

    Changes for streamit server

--- rag/__init__.py
+++ rag/__init__.py
@@ -1,4 +1,7 @@
 import time
+import nltk
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_community.document_loaders import UnstructuredHTMLLoader
